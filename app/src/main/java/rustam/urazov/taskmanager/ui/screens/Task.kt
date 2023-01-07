4550be8eac2560357f24ad580553a1d843a30413
askdh-b <rustpy2703@gmail.com>
2023-01-07 17:49:50 +0600

    task description added

--- app/src/main/java/rustam/urazov/taskmanager/ui/screens/Task.kt
+++ app/src/main/java/rustam/urazov/taskmanager/ui/screens/Task.kt
@@ -21,35 +21,36 @@ import rustam.urazov.taskmanager.ui.theme.*
 
 @Composable
 fun Task(task: TaskView) {
-    BoxWithConstraints(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clip(Shapes.large)
-            .background(White)
-            .padding(LargePaddings)
+    var descriptionEnabled by remember { mutableStateOf(false) }
+
+    BoxWithConstraints(modifier = Modifier
+        .fillMaxWidth()
+        .clip(Shapes.large)
+        .clickable { descriptionEnabled = !descriptionEnabled }
+        .background(White)
+        .padding(LargePaddings)
     ) {
         Column(modifier = Modifier.width(maxWidth - 40.dp)) {
             Text(text = task.name, color = Black, style = Typography.h1)
+
+            if (descriptionEnabled) Text(
+                text = task.description, color = Black, style = Typography.body1
+            )
         }
-        Column(
-            modifier = Modifier
-                .fillMaxWidth(), horizontalAlignment = Alignment.End
-        ) {
+        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.End) {
             var deadlineInfoEnabled by remember { mutableStateOf(false) }
+
             Box(modifier = Modifier.clip(CircleShape)) {
                 Image(
-                    modifier = Modifier.clickable {
-                        deadlineInfoEnabled = !deadlineInfoEnabled
-                    },
+                    modifier = Modifier.clickable { deadlineInfoEnabled = !deadlineInfoEnabled },
                     painter = painterResource(id = R.drawable.ic_baseline_access_time_24),
                     contentDescription = null,
                     alpha = 0.25f
                 )
             }
+
             if (deadlineInfoEnabled) Text(
-                text = task.deadline,
-                color = Black,
-                style = Typography.body1
+                text = task.deadline, color = Black, style = Typography.body1
             )
         }
     }
